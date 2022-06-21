3c9c029ef8f1c7a958a406bf51abc4950fc2cddc
Fran Segui <franciscojsegui@gmail.com>
2022-06-21 19:49:34 -0300

    contacto php

--- Pages/form.php
+++ Pages/form.php
@@ -1,7 +1,6 @@
 <?php
 $nombre = $_POST["name"];
 $apellido = $_POST["apelldio"];
-$edad = $_POST["edad"];
 $tel = $_POST["tel"];
 $mail = $_POST["email"];
 $dia = $_POST["dia"];
@@ -16,7 +15,7 @@ $mensaje .= "Hora a la cuál el cliente asiste" . $hora . ",\r\n";
 
 $para = "franciscojsegui@gmail.com";
 $asunto = "Turno para consultoría";
-header = ("Location index.html");
+header("Location index.html");
 
 mail($para, $asunto, utf8_decode ($mensaje), $header);
 ?>
